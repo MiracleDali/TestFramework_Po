b304f60c0536c60ab403f2a82714aa5c56764053
MiracleDali <108646172+MiracleDali@users.noreply.github.com>
2025-12-03 23:33:30 +0800

    feat(frontend): 添加HTML页面访问与断言功能

--- PageObject/p05_frontend/frontend_html.py
+++ PageObject/p05_frontend/frontend_html.py
@@ -1,3 +1,4 @@
+-- Active: 1761479637360@@192.168.198.128@3306
 import sys
 import pathlib
 Base_dir = pathlib.Path(__file__).resolve().parent.parent.parent
@@ -14,14 +15,22 @@ class FrontendHtml(ApiBase):
         super().__init__(yaml_name='01html')
 
     def get_html_page(self):
-        """ 稿件新增 """
+        """ 访问页面 """
         res = self.request_base(api_name='home_api')
         return res.text
+    
+    def assert_html_page(self, res):
+        """ 1_html页面断言 """
+        logger.info('[断言] html页面断言开始')
+        assert res.find('<title>Index of /Frontend/1_HTML/</title>') != -1, '[断言] html页面断言失败'
+        logger.info('[断言] html页面断言成功')
+
+        
     
 
 
 if __name__ == '__main__':
     fh = FrontendHtml()
     res = fh.get_html_page()
-    print(res)
+    fh.assert_html_page(res)   
     
